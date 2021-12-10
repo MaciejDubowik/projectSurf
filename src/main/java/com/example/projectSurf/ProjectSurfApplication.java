bbbcdef72aa492bc40fd64746da3441f7d3f60b6
MaciejDubowik <maciejxpoland@gmail.com>
2021-12-10 07:35:08 +0100

    .

---
+++ src/main/java/com/example/projectSurf/ProjectSurfApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectSurf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSurfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSurfApplication.class, args);
+	}
+
+}
